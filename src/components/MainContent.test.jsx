9ad7ec855812016bfc750f11a90d04d6a8e18dff
x24ken <x24ken@gmail.com>
2024-01-07 17:34:22 +0900

    test [skip ci]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -14,6 +14,7 @@ describe('MainContent', () => {
   // test
   // Add another comment
   // are?
+  // test
   it('should show the help area after clicking the button', async () => {
     render(<MainContent />);
 
